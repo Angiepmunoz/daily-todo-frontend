8080b5da4b8714499ba249b076c31eafa2083503
Angie Munoz <angiepmunoz@gmail.com>
2023-12-27 16:53:56 -0500

    updated file to includ eif task is an object litwer

--- src/helpers/modifyDate.js
+++ src/helpers/modifyDate.js
@@ -1,10 +1,22 @@
 export function modifyDate(tasks) {
-  return tasks.map((task) => {
-    task.due_date = new Date(`${task.due_date}`).toLocaleDateString("en-gb", {
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-    });
-    return task;
+  if (Array.isArray(tasks)) {
+    return tasks.map(modifyTaskObject);
+  } else {
+    return modifyTaskObject(tasks);
+  }
+}
+
+function modifyTaskObject(task) {
+  task.due_date = new Date(`${task.due_date}`).toLocaleDateString("en-gb", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
   });
+  const timeArr = task.time_of_day.split(":");
+  task.time_of_day =
+    timeArr[0] > 12
+      ? `${timeArr[0] - 12}:${timeArr[1]} PM`
+      : `${timeArr[0]}:${timeArr[1]} AM`;
+
+  return task;
 }
