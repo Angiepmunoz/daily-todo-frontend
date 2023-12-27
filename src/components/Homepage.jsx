f65873de5241e05499f3477091e5ac37524fb194
Angie Munoz <angiepmunoz@gmail.com>
2023-12-27 17:29:21 -0500

    created navigate function to task view

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import { modifyDate } from "../helpers/modifyDate";
 
 // const API = process.env.REACT_APP_API;
 export default function Homepage() {
   const [tasks, setTasks] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API}/todo-list/`).then(({ data }) => {
       const tasksWithModifiedDates = modifyDate(data)  
       setTasks(tasksWithModifiedDates);
     });
   });
+
+  function navigateToTask(id){
+    navigate(`/${id}`)
+  }
 
   return (
     <div>
@@ -25,7 +32,7 @@ export default function Homepage() {
           {tasks.map((task, i) => {
             return (
               <tr key={i}>
-                <td>{task.name}</td>
+                <td onClick={(id)=>navigateToTask(task.id)}>{task.name}</td>
                 <td>{task.due_date}</td>
                 <td>{task.time_of_day}</td>
                 <td>{task.completed ? "✅" : "❌"}</td>
