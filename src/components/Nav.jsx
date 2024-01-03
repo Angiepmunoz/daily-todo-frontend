2da15b0f8b1fe40462bd09634566b6528e534dc0
Angie Munoz <angiepmunoz@gmail.com>
2024-01-03 16:49:38 -0500

    updated css

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+export default function Nav() {
+  return (
+    <nav className="main-page___nav">
+      <div className="main-page___nav-icon">Grind</div>
+      <div className="main-page___nav-link-one">Link</div>
+      <div className="main-page___nav-link-two">Link</div>
+    </nav>
+  );
+}
