7de611261e1cf224f2461a0853bd8d2524fe31f2
Angie Munoz <angiepmunoz@gmail.com>
2024-01-04 21:04:52 -0500

    created quick view modal and css

---
+++ src/components/TaskModal.jsx
@@ -0,0 +1,54 @@
+import { Link, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { modifyDate } from "../helpers/modifyDate";
+import axios from "axios";
+import "../styles/TaskModal.css";
+
+export default function TaskModal({ task }) {
+  // const [task, setTask] = useState({});
+  // const { id } = useParams();
+  // useEffect(() => {
+  //   axios
+  //     .get(`${import.meta.env.VITE_API}/todo-list/${id}`)
+  //     .then(({ data }) => {
+  //       const tasksWithModifiedDate = modifyDate(data);
+  //       setTask(tasksWithModifiedDate);
+  //     });
+  // });
+
+  return (
+    <div className="view-task">
+      <div className="view-task___content">
+        <div className="view-task___header">
+          <h3 className="view-task___header-name">{task.name}</h3>
+          {task.completed ? (
+            <div className="view-task___header-completed">"✅"</div>
+          ) : null}
+          <Link
+            to={`/${task.id}/edit`}
+            className="view-task___header-edit-link"
+          >
+            Edit
+          </Link>
+        </div>
+        <div className="view-task___body">
+          <div className="view-task___body-notes">Notes: {task.notes}</div>
+          <div className="view-task___body-due-date">{task.due_date}</div>
+          <div className="view-task___body-time-due">{task.time_of_day}</div>
+        </div>
+        <div className="view-task___footer">
+          <Link path={`/${task.id}`}>Show More</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// id SERIAL PRIMARY KEY,
+// name TEXT NOT NULL,
+// notes TEXT,
+// completed BOOLEAN DEFAULT false,
+// due_date DATE NOT NULL,
+// time_of_day TIME WITHOUT TIME ZONE NOT null,
+// date_of_completion DATE DEFAULT null,
+// weekly BOOLEAN DEFAULT false
