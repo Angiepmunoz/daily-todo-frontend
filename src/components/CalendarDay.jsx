c8614244b4e9069a80d7fd802a16d852515fe082
Angie Munoz <angiepmunoz@gmail.com>
2024-03-02 17:33:07 -0500

    included map for tasks to populate

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -1,14 +1,15 @@
-export default function CalendarDay({ calendar, index }) {
-  const { month } = calendar;
 
+export default function CalendarDay({ calendar, index, tasksByDay }) {
+
+  const { month,year } = calendar;
   const { cMonth, cToday } = {
     cYear: new Date().getFullYear(),
     cToday: new Date().getDate(),
     cMonth: new Date().getMonth(),
   };
+  const firstDayOfMonth = calendar.firstDayOfMonth();
+  const day = index - firstDayOfMonth + 1;
 
-  let firstDayOfMonth = calendar.firstDayOfMonth();
-  let day = index - firstDayOfMonth + 1;
 
   if (index < firstDayOfMonth || day > calendar.numDaysInMonth()) {
     return <div className="calendar___day-div"></div>;
@@ -16,12 +17,18 @@ export default function CalendarDay({ calendar, index }) {
     return (
       <div className="calendar___day-div">
         <div className="today number">{day}</div>
+        {tasksByDay[day] && tasksByDay[day].map((task,i)=> {
+            return <div key={i}>{task.name}</div>
+        })}
       </div>
     );
   } else {
     return (
       <div className="calendar___day-div">
         <div className="number">{day}</div>
+        {tasksByDay[day] && tasksByDay[day].map((task,i)=> {
+            return <div key={i}>{task.name}</div>
+        })}
       </div>
     );
   }
