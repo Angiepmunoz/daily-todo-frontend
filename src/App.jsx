3ac827d9488d995461eeca1f114ca1ad79e98c04
Angie Munoz <angiepmunoz@gmail.com>
2024-03-02 00:07:19 -0500

    included view options

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,46 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Task from "./components/Task.jsx"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ChecklistRtlOutlinedIcon from "@mui/icons-material/ChecklistRtlOutlined";
+import CalendarMonthOutlinedIcon from "@mui/icons-material/CalendarMonthOutlined";
 import Nav from "./components/Nav.jsx";
 import TodoLists from "./components/TodoLists.jsx";
+import Calendar from "./components/Calendar.jsx";
 import "./App.css";
 
 function App() {
-
+  const [view, setView] = useState("list");
+  function handleViewClick(e) {
+    setView(e.target.id);
+  }
   return (
-      <div className="main-page">
-        <Router>
-          <Nav />
-          <main>
-            <Routes>
-              <Route path="/" element={<TodoLists/>} />
-              <Route path="/:id" element={<Task/>} />
-            </Routes>
-          </main>
-        </Router>
-      </div>
+    <div className="main-page">
+      <Nav />
+      <Router>
+        <main>
+          <div className="main-page___view-choice">
+            <ChecklistRtlOutlinedIcon
+              id="list"
+              className="main-page___view-choice___list"
+              onClick={(e) => handleViewClick(e)}
+            />
+            <CalendarMonthOutlinedIcon
+              id="calendar"
+              className="main-page___view-choice___calendar"
+              onClick={(e) => handleViewClick(e)}
+            />
+          </div>
+          <Routes>
+            {/* <Route path="/" element={<Calendar/>} /> */}
+            <Route
+              path="/"
+              element={view === "list" ? <TodoLists /> : <Calendar />}
+            />
+            {/* <Route path="/:id" element={<Task/>} />
+              <Route path="/:id/edit" element={< Task />} /> */}
+          </Routes>
+        </main>
+      </Router>
+    </div>
   );
 }
 
