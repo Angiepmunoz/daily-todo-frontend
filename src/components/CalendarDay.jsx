a75d2a1d11f94576283bb002f4ec5ea9bc332283
Angie Munoz <angiepmunoz@gmail.com>
2024-03-02 00:13:27 -0500

    moved map return to calendar day component

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -1,24 +1,27 @@
 export default function CalendarDay({ calendar, index }) {
   const { month } = calendar;
+
   const { cMonth, cToday } = {
     cYear: new Date().getFullYear(),
     cToday: new Date().getDate(),
     cMonth: new Date().getMonth(),
   };
+
   let firstDayOfMonth = calendar.firstDayOfMonth();
   let day = index - firstDayOfMonth + 1;
+
   if (index < firstDayOfMonth || day > calendar.numDaysInMonth()) {
     return <div className="calendar___day-div"></div>;
-  } else if (month === cMonth && index === cToday) {
+  } else if (month === cMonth && day === cToday) {
     return (
       <div className="calendar___day-div">
-        <div className="today">{day}</div>
+        <div className="today number">{day}</div>
       </div>
     );
   } else {
     return (
       <div className="calendar___day-div">
-        <div>{day}</div>
+        <div className="number">{day}</div>
       </div>
     );
   }
