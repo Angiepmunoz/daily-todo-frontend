ac631eca6263a90a0008e48639279162724148d3
Angie Munoz <angiepmunoz@gmail.com>
2024-03-01 19:18:15 -0500

    removed modal boolean obj and createInitialModalObj function. It wasn't being used

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -1,34 +1,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { modifyDate } from "../helpers/modifyDate";
+import { modifyDate } from "../utils/modifyDate.js";
 import Task from "./Task.jsx";
 import TaskModal from "./TaskModal.jsx";
 import "../styles/todoLists.css";
 
-// const API = process.env.REACT_APP_API;
+
 export default function TodoLists() {
   const [tasks, setTasks] = useState([]);
-  const [showModal, setShowModal] = useState({});
   const [currentModal, setCurrentModal] = useState({});
   const navigate = useNavigate();
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API}/todo-list/`).then(({ data }) => {
-      const tasksWithModifiedDates = modifyDate(data);
-      setTasks(tasksWithModifiedDates);
-      const modalInitialStateObj = createModalObj(tasksWithModifiedDates);
-      setShowModal(modalInitialStateObj);
+      setTasks(modifyDate(data));
     });
   }, []);
 
-  function createModalObj(tasks) {
-    const taskIdObj = {};
-    tasks.forEach((task) => {
-      taskIdObj[task.id] = false;
-    });
-    return taskIdObj;
-  }
 
   function navigateToTask(id) {
     navigate(`/${id}`);
@@ -42,18 +31,18 @@ export default function TodoLists() {
         <div className="todo-list-table___column-headings">Time of Day</div>
         <div className="todo-list-table___column-headings">Completed</div>
       </div>
-      {tasks.map((task, i) => {
+      {tasks.map((task) => {
         return (
           <Task
             key={task.id}
             task={task}
-            setShowModal={setShowModal}
-            showModal={showModal}
             setCurrentModal={setCurrentModal}
           />
         );
       })}
-      {currentModal.id && <TaskModal task={currentModal.task} setCurrentModal={setCurrentModal}/>}
+      {currentModal.id && (
+        <TaskModal task={currentModal.task} setCurrentModal={setCurrentModal} />
+      )}
     </div>
   );
 }
