78adbdf510e52885b58075a023610ac6d714e08c
Angie Munoz <angiepmunoz@gmail.com>
2024-03-29 15:16:32 -0400

    updated link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
   return (
     <nav className="main-page___nav">
-      <div className="main-page___nav-icon">Grind</div>
+      <div className="main-page___nav-icon"><Link to="/">TickTockTask</Link></div>
       <div className="main-page___nav-link-one">Link</div>
       <div className="main-page___nav-link-two">Link</div>
     </nav>
