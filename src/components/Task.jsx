bd1d0fd3884558eda7b7c24ef94347e14018839b
Angie Munoz <angiepmunoz@gmail.com>
2024-03-29 15:02:18 -0400

    removed unneccessary variables and updated date to formatted variables

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,18 +1,21 @@
-export default function Task({ task, setShowModal, showModal, setCurrentModal}) {
-function handleShowModal(id){
-    setShowModal({...showModal, [id]: true})
-    setCurrentModal({id, task})
-}
+import { useState } from "react";
+export default function Task({ task, setShowModal, showModal, setTaskModal}) {
+  // const [toggleCompletion, setToggleCompletion] = useState()
+  function handleShowModal(id){
+
+    // setShowModal({...showModal, [id]: true})
+    setTaskModal({id, task})
+  }
   return (
     <div className="todo-list-table___column todo-list-table___row">
       <div
-        className="todo-list-table___column-data"
+        className="todo-list-table___column-data task-name"
         onClick={() => handleShowModal(task.id)}
       >
         {task.name}
       </div>
-      <div className="todo-list-table___column-data">{task.due_date}</div>
-      <div className="todo-list-table___column-data">{task.time_of_day}</div>
+      <div className="todo-list-table___column-data">{task.formatted_due_date}</div>
+      <div className="todo-list-table___column-data">{task.formatted_time}</div>
       <div className="todo-list-table___column-data">
         {task.completed ? "✅" : "❌"}
       </div>
