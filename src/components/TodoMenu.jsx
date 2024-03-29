9fb9323a9a4c0eec76182936df22c9f6c7d9c85f
Angie Munoz <angiepmunoz@gmail.com>
2024-03-29 15:21:03 -0400

    created bare bones component with no functionality

---
+++ src/components/TodoMenu.jsx
@@ -0,0 +1,10 @@
+import DragHandleOutlinedIcon from '@mui/icons-material/DragHandleOutlined'; // hamburger menu 
+import FilterListIcon from '@mui/icons-material/FilterList'; //filter icon
+import AddIcon from '@mui/icons-material/Add'; // add icon 
+export default function TodoMenu(){
+
+}
+
+// filter option 
+// add new task 
+// choose view 
