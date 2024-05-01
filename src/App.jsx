991f661cb7b3c2efbf64a6624cc8cce764a5d6bd
Angie Munoz <angiepmunoz@gmail.com>
2024-05-01 16:46:34 -0400

    removed mui variables that were not being used

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Nav from "./components/Nav.jsx";
 import TaskDetails from "./components/TaskDetails.jsx";
 import HomePage from "./pages/HomePage.jsx";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import "./App.css";
 
 function App() {
@@ -12,11 +10,9 @@ function App() {
       <Router>
           <Nav />
           <Routes>
-          {/* <LocalizationProvider dateAdapter={AdapterDayjs}> */}
             <Route path="/" element={<HomePage />} />
             <Route path="/:id" element={<TaskDetails />} />
             {/*<Route path="/:id/edit" element={< Task />} /> */}
-            {/* </LocalizationProvider> */}
           </Routes>
         
       </Router>
